4c8c973a666e52c902d61fd6c64a2727a57f3c83
echobanda <echobanda@gmail.com>
2018-05-14 12:24:10 +0200

    first commit

---
+++ src/Main.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import  Home  from "./Home.jsx";
+import  Services  from "./Services.jsx";
+import  Contact  from "./Contact.jsx";
+import { IndexLink, Link } from 'react-router-dom';
+
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <div>
+          <h1>ECB</h1>
+          <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/services">Services</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/services" component={Services}/>
+            <Route path="/contact" component={Contact}/>
+          </div>
+        </div>
+      </HashRouter>
+    );
+  }
+}
+export default Main;
---
+++ src/Services.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Services extends Component {
+  render() {
+    return (
+      <div>
+        <h2>USŁUGI</h2>
+
+      </div>
+    );
+  }
+}
+
+export default Services;
